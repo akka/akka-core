b0272ea9753f28f53e34a0885b111734cb95394f
Sebastian <sebastian.alfers@gmail.com>
2026-02-16 13:04:32 +0100

    feat: Cluster Sharding telemetry

---
+++ akka-cluster-sharding/src/multi-jvm/scala/akka/cluster/sharding/ClusterShardingInstrumentationSpec.scala
@@ -0,0 +1,248 @@
+/*
+ * Copyright (C) 2019-2025 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.cluster.sharding
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import scala.annotation.nowarn
+import scala.concurrent.duration._
+
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{ Seconds, Span }
+
+import org.scalatest.concurrent.Eventually.eventually
+
+import akka.actor.ActorRef
+import akka.actor.ExtendedActorSystem
+import akka.actor.{ Actor, ActorLogging, Address, Props }
+import akka.cluster.Cluster
+import akka.cluster.MemberStatus
+import akka.cluster.sharding.ClusterShardingInstrumentationSpec.GiveMeYourHome.{ Get, Home }
+import akka.cluster.sharding.internal.{ ClusterShardingInstrumentation, ClusterShardingInstrumentationProvider }
+import akka.remote.testkit.Direction
+import akka.testkit.TestProbe
+import akka.serialization.jackson.CborSerializable
+import akka.testkit.ImplicitSender
+
+object ClusterShardingInstrumentationSpecConfig
+    extends MultiNodeClusterShardingConfig(
+      //loglevel = "DEBUG",
+      additionalConfig = """
+      akka.cluster.sharding {
+        rebalance-interval = 120 s
+        telemetry.instrumentations += akka.cluster.sharding.SpecClusterShardingTelemetry
+        buffer-size = 120
+      }
+     """) {
+
+  val first = role("first")
+  val second = role("second")
+  testTransport(on = true)
+
+}
+
+class ClusterShardingInstrumentationSpecMultiJvmNode1 extends ClusterShardingInstrumentationSpec
+
+class ClusterShardingInstrumentationSpecMultiJvmNode2 extends ClusterShardingInstrumentationSpec
+
+class SpecClusterShardingTelemetry(@nowarn("msg=never used") system: ExtendedActorSystem)
+    extends ClusterShardingInstrumentation {
+
+  val counter = new AtomicInteger(0)
+  val shardHomeRequests = new AtomicInteger(0)
+  val shardHomeResponses = new AtomicInteger(0)
+
+  override def shardRegionBufferSize(
+      selfAddress: Address,
+      shardRegionActor: ActorRef,
+      typeName: String,
+      size: Int): Unit = {
+    counter.set(size)
+  }
+
+  override def incrementShardRegionBufferSize(
+      selfAddress: Address,
+      shardRegionActor: ActorRef,
+      typeName: String): Unit = {
+    counter.incrementAndGet()
+  }
+
+  override def dependencies: Seq[String] = Nil
+
+  override def regionRequestedShardHome(
+      selfAddress: Address,
+      shardRegionActor: ActorRef,
+      typeName: String,
+      shardId: String): Unit =
+    shardHomeRequests.incrementAndGet()
+
+  override def receivedShardHome(
+      selfAddress: Address,
+      shardRegionActor: ActorRef,
+      typeName: String,
+      shardId: String): Unit = shardHomeResponses.incrementAndGet()
+}
+
+object ClusterShardingInstrumentationSpec {
+
+  object GiveMeYourHome {
+    case class Get(id: String) extends CborSerializable
+
+    case class Home(address: Address) extends CborSerializable
+
+    val extractEntityId: ShardRegion.ExtractEntityId = {
+      case g @ Get(id) => (id, g)
+    }
+
+    // shard == id to make testing easier
+    val extractShardId: ShardRegion.ExtractShardId = {
+      case Get(id) => id
+      case _       => throw new IllegalArgumentException()
+    }
+  }
+
+  class GiveMeYourHome extends Actor with ActorLogging {
+
+    val selfAddress = Cluster(context.system).selfAddress
+
+    log.info("Started on {}", selfAddress)
+
+    override def receive: Receive = {
+      case Get(_) =>
+        sender() ! Home(selfAddress)
+    }
+  }
+}
+
+abstract class ClusterShardingInstrumentationSpec
+    extends MultiNodeClusterShardingSpec(ClusterShardingInstrumentationSpecConfig)
+    with ImplicitSender
+    with ScalaFutures {
+
+  import ClusterShardingInstrumentationSpec._
+  import ClusterShardingInstrumentationSpec.GiveMeYourHome._
+  import ClusterShardingInstrumentationSpecConfig._
+
+  private val counter = ClusterShardingInstrumentationProvider(system).instrumentation
+    .asInstanceOf[SpecClusterShardingTelemetry]
+    .counter
+
+  val shardHomeRequests =
+    ClusterShardingInstrumentationProvider(system).instrumentation
+      .asInstanceOf[SpecClusterShardingTelemetry]
+      .shardHomeRequests
+
+  val shardHomeResponses =
+    ClusterShardingInstrumentationProvider(system).instrumentation
+      .asInstanceOf[SpecClusterShardingTelemetry]
+      .shardHomeResponses
+
+  override implicit val patienceConfig: PatienceConfig = {
+    import akka.testkit.TestDuration
+    PatienceConfig(testKitSettings.DefaultTimeout.duration.dilated, Span(1000, org.scalatest.time.Millis))
+  }
+
+  val typeName = "GiveMeYourHome"
+
+  def shardRegion =
+    startSharding(
+      system,
+      typeName = typeName,
+      entityProps = Props[GiveMeYourHome](),
+      extractEntityId = extractEntityId,
+      extractShardId = extractShardId)
+
+  "External shard allocation" must {
+    "join cluster" in within(20.seconds) {
+
+      join(first, first, onJoinedRunOnFrom = shardRegion)
+      join(second, first, onJoinedRunOnFrom = shardRegion, assertNodeUp = false)
+
+      // all Up, everywhere before continuing
+      runOn(first, second) {
+        awaitAssert {
+          cluster.state.members.size should ===(2)
+          cluster.state.members.unsorted.map(_.status) should ===(Set(MemberStatus.Up))
+        }
+      }
+
+      enterBarrier("after-2")
+    }
+
+    "default to allocating a shard to the local shard region" in {
+      runOn(first, second) {
+
+        shardRegion ! Get("a")
+        val address1 = expectMsgType[GiveMeYourHome.Home].address
+        shardRegion ! Get("b")
+        val address2 = expectMsgType[GiveMeYourHome.Home].address
+        shardRegion ! Get("c")
+        val address3 = expectMsgType[GiveMeYourHome.Home].address
+
+        log.info(s"<<<<< ${List(address1, address2, address3)}")
+      }
+      enterBarrier("local-message-sent")
+    }
+
+    "cut traffic to coordinator" in {
+      runOn(first) {
+        testConductor.blackhole(first, second, Direction.Both).await
+      }
+      enterBarrier("blackhole-created")
+    }
+
+    "start shards to trigger buffering" in {
+      runOn(second) {
+        val probe = TestProbe()
+        (1 to 100).foreach { n =>
+          shardRegion.tell(Get(s"id-$n"), probe.ref)
+        }
+        eventually {
+          counter.get() shouldBe 100
+        }
+      }
+      enterBarrier("messages-buffered")
+    }
+
+    "let the buffer overflow" in {
+      runOn(second) {
+        val probe = TestProbe()
+        (1 to 30).foreach { n =>
+          shardRegion.tell(Get(s"id-$n"), probe.ref)
+        }
+        eventually {
+          // we have 100 in the buffer, and our cap is 120 (per config in this test)
+          // 10 are dropped. Should we have a metric on this? Or custom events?
+          counter.get() shouldBe 120
+        }
+      }
+      enterBarrier("buffer-overflow")
+    }
+
+    "resume traffic to coordinator" in {
+      runOn(first) {
+        testConductor.passThrough(first, second, Direction.Both).await
+      }
+      enterBarrier("blackhole-removed")
+    }
+
+    "record latency of requesting ShardHome" in {
+      runOn(second) {
+        shardHomeRequests.get() shouldBe shardHomeResponses.get()
+      }
+      enterBarrier("measure-shard-home-latency")
+    }
+
+    "clear buffered messages" in {
+      runOn(second) {
+        eventually(timeout(Span(5, Seconds))) {
+          counter.get() shouldBe 0
+        }
+      }
+      enterBarrier("messages-buffered-cleared")
+    }
+
+  }
+}
