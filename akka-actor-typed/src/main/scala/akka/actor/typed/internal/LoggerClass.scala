9064514331223dcce1f5b2ce79d3caf2e724a6f3
Jason Zaugg <jzaugg@gmail.com>
2026-04-28 23:53:05 +1000

    perf: use StackWalker for logger class detection (#32917)
    * Replace legacy SecurityManager-based stack walking with Java 9+ StackWalker API
    * Improve performance by lazily streaming stack frames instead of capturing full class context
    * Remove TrickySecurityManager internal utility which is no longer needed
    * Add JUnit test case to verify logger detection through indirect call frames

--- akka-actor-typed/src/main/scala/akka/actor/typed/internal/LoggerClass.scala
+++ akka-actor-typed/src/main/scala/akka/actor/typed/internal/LoggerClass.scala
@@ -5,50 +5,28 @@
 package akka.actor.typed.internal
 
 import scala.util.control.NonFatal
-
 import akka.annotation.InternalApi
-import akka.util.OptionVal
+import java.lang.StackWalker.Option.RETAIN_CLASS_REFERENCE
 
 /**
  * INTERNAL API
  */
 @InternalApi
 private[akka] object LoggerClass {
-
-  // just to get access to the class context
-  private final class TrickySecurityManager extends SecurityManager {
-    def getClassStack: Array[Class[_]] = getClassContext
-  }
-
   private val defaultPrefixesToSkip = List("scala.runtime", "akka.actor.typed.internal")
 
   /**
    * Try to extract a logger class from the call stack, if not possible the provided default is used
    */
   def detectLoggerClassFromStack(default: Class[_], additionalPrefixesToSkip: List[String] = Nil): Class[_] = {
-    // TODO use stack walker API when we no longer need to support Java 8
     try {
-      def skip(name: String): Boolean = {
-        def loop(skipList: List[String]): Boolean = skipList match {
-          case Nil => false
-          case head :: tail =>
-            if (name.startsWith(head)) true
-            else loop(tail)
-        }
+      val walker = StackWalker.getInstance(RETAIN_CLASS_REFERENCE)
+      val skipList = additionalPrefixesToSkip ::: defaultPrefixesToSkip
+      def isInternal(clazz: Class[_]) = skipList.exists(clazz.getName.startsWith)
 
-        loop(additionalPrefixesToSkip ::: defaultPrefixesToSkip)
+      walker.walk { frames =>
+        frames.map[Class[_]](f => f.getDeclaringClass).filter(cls => !isInternal(cls)).findFirst().orElse(default)
       }
-
-      val trace = new TrickySecurityManager().getClassStack
-      var suitableClass: OptionVal[Class[_]] = OptionVal.None
-      var idx = 1 // skip this method/class and right away
-      while (suitableClass.isEmpty && idx < trace.length) {
-        val clazz = trace(idx)
-        val name = clazz.getName
-        if (!skip(name)) suitableClass = OptionVal.Some(clazz)
-        idx += 1
-      }
-      suitableClass.getOrElse(default)
     } catch {
       case NonFatal(_) => default
     }
