2e16fee0ca244dbf374363c89f17de79bf868eb6
Johan Andrén <johan@markatta.com>
2026-08-07 10:48:33 +0200

    fix it in the extractor adapter instead (#32959)

--- akka-cluster-sharding-typed/src/main/scala/akka/cluster/sharding/typed/internal/ClusterShardingImpl.scala
+++ akka-cluster-sharding-typed/src/main/scala/akka/cluster/sharding/typed/internal/ClusterShardingImpl.scala
@@ -33,6 +33,7 @@ import akka.cluster.sharding.ShardCoordinator
 import akka.cluster.sharding.ShardCoordinator.ShardAllocationStrategy
 import akka.cluster.sharding.ShardRegion
 import akka.cluster.sharding.ShardRegion.{ StartEntity => ClassicStartEntity }
+import akka.cluster.sharding.internal.RememberEntitiesShardStore
 import akka.cluster.sharding.typed.scaladsl.EntityContext
 import akka.cluster.typed.Cluster
 import akka.event.Logging
@@ -48,14 +49,20 @@ import akka.util.JavaDurationConverters._
  * INTERNAL API
  * Extracts entityId and unwraps ShardingEnvelope and StartEntity messages.
  * Other messages are delegated to the given `ShardingMessageExtractor`.
+ *
+ * Internal remember-entities store messages (e.g. a stale reply arriving after the shard already
+ * restarted due to a timed out write) must never reach the user-supplied extractor: they are not of
+ * type `E`, so delegating them would throw a `ClassCastException` instead of simply not matching.
  */
 @InternalApi private[akka] class ExtractorAdapter[E, M](delegate: ShardingMessageExtractor[E, M])
     extends ShardingMessageExtractor[Any, M] {
   override def entityId(message: Any): String = {
     message match {
-      case ShardingEnvelope(entityId, _) => entityId //also covers ClassicStartEntity in ShardingEnvelope
-      case ClassicStartEntity(entityId)  => entityId
-      case msg                           => delegate.entityId(msg.asInstanceOf[E])
+      case ShardingEnvelope(entityId, _)                    => entityId //also covers ClassicStartEntity in ShardingEnvelope
+      case ClassicStartEntity(entityId)                     => entityId
+      case _: RememberEntitiesShardStore.UpdateDone         => null
+      case _: RememberEntitiesShardStore.RememberedEntities => null
+      case msg                                              => delegate.entityId(msg.asInstanceOf[E])
     }
   }
 
@@ -69,6 +76,8 @@ import akka.util.JavaDurationConverters._
       case msg: ClassicStartEntity =>
         // not really of type M, but erased and StartEntity is only handled internally, not delivered to the entity
         msg.asInstanceOf[M]
+      case _: RememberEntitiesShardStore.UpdateDone         => null.asInstanceOf[M]
+      case _: RememberEntitiesShardStore.RememberedEntities => null.asInstanceOf[M]
       case msg =>
         delegate.unwrapMessage(msg.asInstanceOf[E])
     }
